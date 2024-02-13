cb23af40c6ced2eb06a9c9c200504163a581c867
dds29373 <dds29373@11MC000045253>
2024-02-12 19:54:54 -0500

    Add project files.

---
+++ D.S. Week 4/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.S.Week_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("a for Doctor - Sick Appointment");
+            Console.WriteLine("b for Doctor - Check-up");
+
+            char choice = Console.ReadKey().KeyChar;
+            Console.WriteLine();
+
+            switch (choice)
+            {
+                case 'a':
+                    SickAppointment();
+                    break;
+                case 'b':
+                    CheckUp();
+                    break;
+                default:
+                    Console.WriteLine("Invalid!");
+                    break;
+            }
+        }
+
+        static void SickAppointment()
+        {
+            Console.WriteLine("Is the patient a child or an adult? (Enter 'child' or 'adult')");
+            string patientType = Console.ReadLine();
+
+            double baseCharge;
+            if (patientType == "child")
+                baseCharge = 50;
+            else if (patientType == "adult")
+                baseCharge = 75;
+            else
+            {
+                Console.WriteLine("Invalid!");
+                return;
+            }
+
+            Console.WriteLine("Were labs done? (Enter 'yes' or 'no')");
+            string labsDone = Console.ReadLine();
+
+            double additionalCharge = 0;
+            if (labsDone == "yes")
+                additionalCharge = 25;
+
+            double totalCharge = baseCharge + additionalCharge;
+
+            Console.WriteLine($"Total charge for the appointment: {totalCharge:C}");
+            Console.ReadKey();
+        }
+
+        static void CheckUp()
+        {
+            Console.WriteLine("Is the patient a child or an adult? (Enter 'child' or 'adult')");
+            string patientType = Console.ReadLine();
+
+            double charge;
+            if (patientType == "child")
+                charge = 75;
+            else if (patientType == "adult")
+                charge = 100;
+            else
+            {
+                Console.WriteLine("Please select an option");
+                return;
+            }
+
+            Console.WriteLine($"The charge for the check-up: {charge:C}");
+            Console.ReadKey();
+      }  }
+    }
---
+++ D.S. Week 4/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("D.S. Week 4")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Reynolds Community College")]
+[assembly: AssemblyProduct("D.S. Week 4")]
+[assembly: AssemblyCopyright("Copyright © Reynolds Community College 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("6281b949-c60f-4c3d-b6f6-3db9fa8b6554")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
